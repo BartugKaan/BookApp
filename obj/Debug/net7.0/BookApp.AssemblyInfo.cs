23f1cabd5cbb126a35edbd8b68b9c297acf74a7b
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-10-09 01:16:00 +0300

    add: Create book view

--- obj/Debug/net7.0/BookApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/BookApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9c68837552bed88a55af9726387f1ff98a61b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eabf6acd1a004eb385d706371ad611c44067938")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
